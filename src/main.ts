6e0c11903ea20b51ce73c96788edadd07da165dd
Davide <davide.cuccato5@gmail.com>
2022-09-03 00:52:42 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ const exposePort = isProd ? 80 : 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // CORS
+  app.enableCors()
+
   await app.listen(exposePort)
+
+  console.log(
+    `Application is running on: ${await app.getUrl()} | ${exposePort} | ENV: ${
+      process.env.NODE_ENV
+    }`
+  )
 }
 bootstrap()
